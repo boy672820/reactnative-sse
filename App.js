673b9d0612340fe1bfd6c12a5521554273a4f0a5
이선주 <seondev@iseonjuui-MacBookPro.local>
2022-02-18 14:40:06 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import react, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import EventSource from 'react-native-sse';
 
 export default function App() {
+  const [data, setData] = react.useState(null);
+
+  useEffect(() => {
+    const es = new EventSource('http://192.168.2.29:8112/wallet/event');
+    es.addEventListener('message', (event) => {
+      setData(event.data);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
+      {data && <Text>New message: {data}</Text>}
+      <StatusBar style='auto' />
     </View>
   );
 }
