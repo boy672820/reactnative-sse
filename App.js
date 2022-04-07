a9d01ed963a26ae1ad149dc84b70cfad5288313c
seonzoo <boy672820@gmail.com>
2022-04-07 19:09:14 +0900

    지갑 생성 실패 시 예외처리(event)

--- App.js
+++ App.js
@@ -7,15 +7,29 @@ export default function App() {
   const [data, setData] = react.useState(null);
 
   useEffect(() => {
-    const es = new EventSource('http://192.168.2.29:8112/wallet/event');
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3YjhjNDFhNmQwMzU3Yjk3OWU4Mjc0MjQ4ZWRjN2Y3MTo5OTZkZWRlZTU1YjVhNDkzYzkiLCJpYXQiOjE2NDU0MTY5NDIsImV4cCI6MTY0NzI3Njk0Mn0.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3YjhjNDFhNmQwMzU3Yjk3OWU4Mjc0MjQ4ZWRjN2Y3MTo5OTZkZWRlZTU1YjVhNDkzYzkiLCJpYXQiOjE2NDU0MTY5NDIsImV4cCI6MTY0NzI3Njk0Mn0.aifkPS1zNrMesKztozsav4zyEsJ60nUABBnCnrabLBw';
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const es = new EventSource('http://127.0.0.1:8113/wallet/event', config);
+
+    es.addEventListener('open', (event) => {
+      console.log(event);
+    });
+
     es.addEventListener('message', (event) => {
+      console.log(event);
       setData(event.data);
     });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Hello</Text>
       {data && <Text>New message: {data}</Text>}
       <StatusBar style='auto' />
     </View>
